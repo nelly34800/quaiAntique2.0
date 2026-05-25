2df5e290d29c7cf7ec7065900527eeb19bc32ff1
nelly34800 <nellyboussekane@free.fr>
2026-05-25 16:51:13 +0200

    ajout des pages statiques home, galerie et  la carte

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -2,6 +2,9 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "./pages/home.html", []),
+  new Route("/galerie", "Galerie", "./pages/galerie.html", []),
+  new Route("/carte", "La carte", "./pages/carte.html", [])
+
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
