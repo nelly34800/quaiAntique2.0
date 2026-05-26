0b3dd0df1c1e7fc0ec8a6be881866685760664df
nelly34800 <nellyboussekane@free.fr>
2026-05-26 18:46:05 +0200

    création  de la gestion de compte (dossier auth avec signin, signup, account...)

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -3,7 +3,11 @@ import Route from "./route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "./pages/home.html", []),
   new Route("/galerie", "Galerie", "./pages/galerie.html", []),
-  new Route("/carte", "La carte", "./pages/carte.html", [])
+  new Route("/carte", "La carte", "./pages/carte.html", []),
+  new Route("/signin", "Connexion", "./pages/auth/signin.html", []),
+  new Route("/signup", "Inscription", "./pages/auth/signup.html", []),
+  new Route("/account", "Mon compte", "./pages/auth/account.html", []),
+  new Route("/editPassword", "Changer mot de passe", "./pages/auth/editPassword.html", [])
 
 ];
 
