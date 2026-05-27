99d906641903a74cca2c48cdc5e28282cd9c6af8
nelly34800 <nellyboussekane@free.fr>
2026-05-27 16:42:28 +0200

    ajout des boutons + modale de la gallerie + ajout des pages de réservations + renommage des pages pour pour cohérence (tout en anglais)

--- frontend/Router/allRoutes.js
+++ frontend/Router/allRoutes.js
@@ -2,13 +2,14 @@ import Route from "./route.js";
 //Définir ici vos routes
 export const allRoutes = [
   new Route("/", "Accueil", "./pages/home.html", []),
-  new Route("/galerie", "Galerie", "./pages/galerie.html", []),
-  new Route("/carte", "La carte", "./pages/carte.html", []),
+  new Route("/galery", "Galerie", "./pages/galery.html", []),
+  new Route("/menu", "La carte", "./pages/menu.html", []),
   new Route("/signin", "Connexion", "./pages/auth/signin.html", []),
   new Route("/signup", "Inscription", "./pages/auth/signup.html", []),
   new Route("/account", "Mon compte", "./pages/auth/account.html", []),
-  new Route("/editPassword", "Changer mot de passe", "./pages/auth/editPassword.html", [])
-
+  new Route("/editPassword", "Changer mot de passe", "./pages/auth/editPassword.html", []),
+  new Route("/allReservations", "Vos réservations", "./pages/reservations/allReservations.html", []),
+  new Route("/reserve", "Réserver", "./pages/reservations/reserve.html", []),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
